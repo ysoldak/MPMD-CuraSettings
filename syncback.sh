0feb326615a3a6bbc80498bd385506d3fcec3961
Yurii Soldak <ysoldak@gmail.com>
2018-04-11 00:26:01 +0200

    selective syncback and better output

--- syncback.sh
+++ syncback.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 VERSION=${1:-3.2}
+TYPE=${2:-all}
 
 pgrep -q cura
 if [ "$?" == "0" ]; then
@@ -11,13 +12,20 @@ fi
 # TODO make work on linux also
 CURA_HOME="${HOME}/Library/Application Support/cura/${VERSION}"
 
-echo "Syncing back machine changes from cura dir: ${CURA_HOME}"
+echo "Syncing back machine changes from cura dir"
+echo "   ${CURA_HOME}"
+echo 
 
-for file in `find -E . -regex ".*\.(cfg|stl|json)"`; do
+for file in `find -E . -regex ".*\.(cfg|stl|json)" | sort`; do
     dirname=`dirname $file | sed s,\./,,`
     basename=`basename $file`
-    echo "Syncing ${basename}"
-    cp "${CURA_HOME}/${dirname}/${basename}" "`pwd`/${dirname}/${basename}"
+    if [ "${TYPE}" == "all" ] || [ "${TYPE}" == "${dirname}" ]; then
+        echo "  < ${basename}"
+        cp "${CURA_HOME}/${dirname}/${basename}" "`pwd`/${dirname}/${basename}"
+    else
+        echo "  ~ ${basename}"
+    fi
 done
 
+echo
 echo "Done!"
