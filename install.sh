fc99923ebd4c25706a726d6019a8d4465c00493e
Yurii Soldak <ysoldak@gmail.com>
2018-04-11 00:26:37 +0200

    Better output

--- install.sh
+++ install.sh
@@ -11,22 +11,26 @@ fi
 # TODO make work on linux also
 CURA_HOME="${HOME}/Library/Application Support/cura/${VERSION}"
 
-echo "Installing machine into cura dir: ${CURA_HOME}"
+echo "Installing machine into cura dir"
+echo "  ${CURA_HOME}"
+echo
 
 for file in `find -E . -regex ".*\.(cfg|stl|json)"`; do
     dirname=`dirname $file | sed s,\./,,`
     basename=`basename $file`
-    echo "Installing ${basename}"
     mkdir -p "${CURA_HOME}/${dirname}"
     if [ "${dirname}" == "definition_changes" ]; then
         # force install start/end gcode definitions
+        echo "  > ${basename}"
         cp "`pwd`/${dirname}/${basename}" "${CURA_HOME}/${dirname}/${basename}"
     else
         # do not overwrite file at destination to preserve user settings, quality selected, etc
+        echo "  ? ${basename}"
         cp -n "`pwd`/${dirname}/${basename}" "${CURA_HOME}/${dirname}/${basename}"
     fi
     # Linking does not work since Cura re-creates configuration files instead of updating them
     #ln -s `pwd`/${dirname}/${basename} "${CURA_HOME}/${dirname}/${basename}"
 done
 
+echo
 echo "Done!"
--- uninstall.sh
+++ uninstall.sh
@@ -8,15 +8,19 @@ if [ "$?" == "0" ]; then
     exit 1
 fi
 
+# TODO make work on linux also
 CURA_HOME="${HOME}/Library/Application Support/cura/${VERSION}"
 
-echo "Uninstalling machine from cura dir: ${CURA_HOME}"
+echo "Uninstalling machine from cura dir"
+echo "  ${CURA_HOME}"
+echo
 
 for file in `find -E . -regex ".*\.(cfg|stl|json)"`; do
     dirname=`dirname $file | sed s,\./,,`
     basename=`basename $file`
-    echo "Removing ${basename}"
+    echo "  x ${basename}"
     rm -f "${CURA_HOME}/${dirname}/${basename}"
 done
 
+echo
 echo "Done!"
